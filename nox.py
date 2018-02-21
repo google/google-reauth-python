5d68279223978c903f28327bbccc416d2870d08e
Daria Kashcha <dariakashcha@dariakashcha-linuxstation.kir.corp.google.com>
2018-02-20 17:05:21 -0800

    Implementation for the reauth and oauth2client credentials that support reauth

--- nox.py
+++ nox.py
@@ -18,17 +18,16 @@ import nox
 
 
 @nox.session
-@nox.parametrize('py', ['2.7', '3.4', '3.5', '3.6'])
+@nox.parametrize('py', ['2.7'])
 def tests(session, py):
     session.interpreter = 'python{}'.format(py)
-    session.install('mock', 'pytest', 'pytest-cov')
+    session.install('mock', 'pytest', 'pytest-cov', 'oauth2client', 'pyu2f')
     session.install('-e', '.')
 
     session.run(
         'py.test',
         '--quiet',
         '--cov=google_reauth',
-        '--cov-erase',
         '--cov-config=.coveragerc',
         'tests',
         *session.posargs
